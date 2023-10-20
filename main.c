796686e40789a11b80843582f004165592b8b05b
Hyooojeong <hjj001028@naver.com>
2023-10-20 21:51:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	int i;
-	int temp = 1;
+int main(void) 
+{
 	
-	for (i=0; i<5; i++)
-	{
-		printf("temp = %d\n", temp);
-		temp++;
-	}
+	int i;
+	printf("함수 호출전 i = %d\n", i);
+	inc(i);
+	printf("함수 호출후 i = %d\n", i);
 	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
